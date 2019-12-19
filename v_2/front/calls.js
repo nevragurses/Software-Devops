718484349647b4c40c496665da3886825157ace8
uderbashi <uderbashi@gtu.edu.tr>
2019-12-19 22:08:25 +0300

    Yine V2 icin guncellemeler, 0- public dir'in ismini degistirdim front'a daha net olsun onun nedeni 1- Back end'i ekledim 2- Back end'e gelen POST request'i handle ediyorum 3- Kodu biraz temizledim okuma icin daha rahat olsun

--- v_2/front/calls.js
+++ v_2/front/calls.js
@@ -1,11 +1,12 @@
-var socket = io.connect('http://localhost:4000');
-
-
+var socket = io.connect('http://localhost:4001');
 
 document.getElementById("UndeployButton").addEventListener('click', function(){
 	socket.emit('undeploy', {
 		ProjectName: document.getElementById("ProjectName").value
 	});
 	document.getElementById("ProjectName").value = "";
-}); 
+});
 
+socket.on('postIncoming', function(data){
+	document.getElementById("ProjectName").value = "got the value " + data.hi + " as an argument of \"hi\" in POST";
+});
--- v_2/index.js
+++ v_2/index.js
@@ -3,14 +3,28 @@ var socket = require('socket.io');
 var request = require('request');
 
 
+
+//*************************//
+//Ports and their listeners//
+//*************************//
 var front = express();
-var back = front.listen(4000, function(){
-	console.log("listening on 4000")
+var frontListener = front.listen(4001, function(){
+	console.log("localhost:4001 to access the front end")
 });
+front.use(express.static('front'));
 
-front.use(express.static('public'));
+var back = express();
+var backListener = back.listen(4000, function(){
+	console.log("other groups send to localhost:4000")
+});
+back.use(express.json());
 
-var io = socket(back);
+
+
+//*****************//
+//Frontend's socket//
+//*****************//
+var io = socket(frontListener);
 
 io.on('connection', function(socket){
 	socket.on('undeploy', function(data){
@@ -18,6 +32,16 @@ io.on('connection', function(socket){
 	});
 });
 
+back.post('/', function(req, res){
+	io.emit('postIncoming', req.body);
+	res.send(req.body);
+});	
+
+
+
+//****************//
+//Helper Functions//
+//****************//
 function undeployPost(jsonData){
 	request.post(
 		'http://localhost:8081',
